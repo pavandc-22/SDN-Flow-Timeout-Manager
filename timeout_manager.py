e8e12e45a8361480bb4044b89e866c7510540a2f
Pavan DC <pavandc2006@gmail.com>
2026-04-10 16:39:05 +0530

    Update timeout_manager.py

--- timeout_manager.py
+++ timeout_manager.py
@@ -14,26 +14,37 @@ class TimeoutManager(object):
         # Create flow rule
         msg = of.ofp_flow_mod()
 
+        # Match incoming packet
         msg.match = of.ofp_match.from_packet(packet, event.port)
 
-        # 🔥 TIMEOUT SETTINGS
+        # TIMEOUT SETTINGS
         msg.idle_timeout = 10   # removed if idle for 10 sec
-        msg.hard_timeout = 30   # removed after 30 sec anyway
+        msg.hard_timeout = 30   # removed after 30 sec
 
+        # Action: flood packet
         msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
 
-        # Send flow rule
+        # Send flow rule to switch
         self.connection.send(msg)
 
-        log.info("Flow installed with timeout")
+        # IMPORTANT: Send current packet immediately
+        packet_out = of.ofp_packet_out()
+        packet_out.data = event.ofp
+        packet_out.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        self.connection.send(packet_out)
+
+        log.info("Flow installed with idle_timeout=10, hard_timeout=30")
+
 
 class TimeoutController(object):
     def __init__(self):
         core.openflow.addListeners(self)
 
     def _handle_ConnectionUp(self, event):
-        log.info("Switch connected")
+        log.info("Switch connected: %s", event.connection)
         TimeoutManager(event.connection)
 
+
 def launch():
+    log.info("Starting Flow Rule Timeout Manager Controller")
     core.registerNew(TimeoutController)
